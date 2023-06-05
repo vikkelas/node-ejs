940ed1e7321aec91011a411b981156fa69e486cf
vikkelas <bass89@mail.ru>
2023-06-06 00:40:05 +0300

    finish

--- public/js/submit.js
+++ public/js/submit.js
@@ -2,10 +2,19 @@ const form = document.querySelector('.myForm');
 if(form){
     form.addEventListener('submit',(e)=>{
         e.preventDefault()
-        const formData = new FormData(form);
-        fetch('http://localhost:3001/', {
-            method: "POST",
-            body: formData
-        }).then(r=>window.location.href = r.url)
+        const id = e.currentTarget.dataset.id
+        if(!id){
+            const formData = new FormData(form);
+            fetch('http://localhost:3001/', {
+                method: "POST",
+                body: formData
+            }).then(r=>window.location.href = r.url)
+        }else{
+            const formData = new FormData(form);
+            fetch(`http://localhost:3001/update/${id}`, {
+                method: "PUT",
+                body: formData
+            }).then(r=>window.location.href = r.url)
+        }
     })
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,17 @@ const store = require('../store')
 
 router.get('/', (req, res)=>{
     res.render("index",{
-        title: 'Главная'
+        title: 'Главная',
+        books: store
+    })
+})
+
+router.get('/book/:id',(req, res)=>{
+    const {id} = req.params;
+    const book = store.find(i=>i.id===id);
+    res.render("view", {
+        title: book.title,
+        book
     })
 })
 
@@ -16,11 +26,51 @@ router.post('/',(req, res)=>{
     res.redirect('/')
 })
 
+router.get('/update/:id', (req, res)=>{
+    const {id} = req.params;
+    const book = store.find(i=>i.id===id);
+    if(!book){
+        res.redirect('/404')
+    }
+    res.render("update", {
+        title: 'Изменить',
+        btn: 'Изменить',
+        book
+    })
+})
+
+router.put('/update/:id', (req, res)=> {
+    const {id} = req.params;
+    const book = store.findIndex(i=>i.id===id);
+    if(book===-1){
+        res.redirect('/404')
+    }
+    const changeObj = req.fields
+    for (let key in changeObj){
+        if(key==='favorite'){
+            store[book]["favorite"]=!!changeObj.favorite
+        }
+        store[book][key]=changeObj[key]
+    }
+    res.redirect(`/book/${id}`)
+})
 
 router.get('/new',(req, res)=>{
-    res.render("newBook",{
-        title: 'Добавить книгу'
+    res.render("create",{
+        title: 'Добавить книгу',
+        btn: 'Создать',
+        book: {}
     })
+})
+
+router.get('/delete/:id',(req, res)=>{
+    const {id} = req.params;
+    const book = store.findIndex(i=>i.id===id);
+    if(book===-1){
+        res.redirect('/404')
+    }
+    store.splice(book,1)
+    res.redirect(`/`)
 })
 
 module.exports = router;
